34f8cdef452067178d26749f8aa9609b37891b07
Addilyn <addilyz@protonmail.com>
2025-05-08 00:20:39 -0700

    still working lol

--- codex.lua
+++ codex.lua
@@ -54,3 +54,30 @@ function codex.add(key,tab)
 		if codex[k] then codex[k][key] = v end
 	end
 end
+
+function love.run()
+	codex.handle("load",love.arg.parseGameArguments(arg), arg)
+	local dt = 0
+	return function()
+		if love.event then
+			love.event.pump()
+			for name, a,b,c,d,e,f in love.event.poll() do
+				if name == "quit" then
+					if not love.quit or not love.quit() then
+						return a or 0
+					end
+				end
+				codex.handle(name,a,b,c,d,e,f)
+			end
+		end
+		if love.timer then dt = love.timer.step() end
+		codex.handle("update",dt)
+		if love.graphics and love.graphics.isActive() then
+			love.graphics.origin()
+			love.graphics.clear(love.graphics.getBackgroundColor())
+			codex.handle("draw")
+			love.graphics.present()
+		end
+		if love.timer then love.timer.sleep(0.001) end
+	end
+end
