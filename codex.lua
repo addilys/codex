54822c11d36399f37ffb8907f297a27eb797741c
Addilyn McAllister <109492195+addilyz@users.noreply.github.com>
2025-05-21 07:35:35 -0700

    pages and stuff

--- codex.lua
+++ codex.lua
@@ -1,26 +1,3 @@
---[[ codex by addilyz
-MIT License
-
-Copyright (c) 2025 addilyz
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-]]
 
 codex = {
 	draw = {},
@@ -57,6 +34,7 @@ codex = {
 	touchpressed = {},
 	touchreleased = {}
 }
+codex.pages = require("codex/pages")
 
 function codex.handle(name,a,b,c,d,e,f)
 	for k, v in next, codex[name] do
@@ -111,7 +89,7 @@ function love.run()
 		if love.graphics and love.graphics.isActive() then
 			love.graphics.origin()
 			love.graphics.clear(love.graphics.getBackgroundColor())
-			codex.handle("draw")
+			codex.pages.draw()
 			love.graphics.present()
 		end
 		if love.timer then love.timer.sleep(0.001) end
